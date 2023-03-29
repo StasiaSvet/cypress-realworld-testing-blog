1ac07b49d60863c85b0be80f1fa90517a52d3e60
StessyT <wildorhid@gmail.com>
2023-03-30 09:31:28 +1100

    Added API tests check

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,14 @@ module.exports = defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      baseUrl: "http://localhost:3000",
+      specPattern: "cypress/e2e/**/*.*.{js,jsx,ts,tsx}",
+      viewportHeight: 1000,
+      viewportWidth: 1280,
+      setupNodeEvents(on, config) {
+        return require('./cypress/plugins/index.js')(on, config)
+        // implement node event listeners here
+      },
+      
     },
-  },
-})
+  })
